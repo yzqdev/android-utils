2592bf4e610b20971e1bc81a9c5dbf0f57fd39ec
yzqdev <yzqdev@outlook.com>
2023-04-01 18:39:39 +0800

    add network util, 2023-04-01 18:39:33

---
+++ sample/src/main/java/com/yzq/utilssample/MainActivity.java
@@ -0,0 +1,53 @@
+package com.yzq.utilssample;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.yzq.androidutils.NetWorkUtils;
+import com.yzq.utilssample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+        if (!NetWorkUtils.isConnected(this)){
+            AlertDialog networkDlg= new AlertDialog.Builder(this)
+                    .setTitle(R.string.hint)//标题
+                    .setMessage(R.string.no_network)//内容
+                    .setPositiveButton(R.string.confirm, (dialog, which) -> {
+
+                    })
+                    .create();
+            networkDlg.show();
+        }
+
+
+
+    }
+
+}
